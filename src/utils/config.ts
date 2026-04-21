3854ccbd32a64bffa172050acccc64c5c3d7ac35
Subham Thapa <subhamits4.2@gmail.com>
2026-04-21 15:29:42 +0545

    config update

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,7 +2,7 @@ export const config = {
   RABBITMQ_URL: process.env.RABBITMQ_URL || "amqp://localhost:5672",
   MONGODB_URI:
     process.env.MONGODB_URI ??
-    "mongodb://root:example@localhost:27021/video_procssing",
+    "mongodb://root:example@localhost:27021/video_processing",
   VIDEO_PROCESS_QUEUE:
     process.env.VIDEO_PROCESS_QUEUE ?? "video.process",
 };
